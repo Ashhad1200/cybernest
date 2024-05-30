8ee30c3d0ccc5b07f04f2c96e784228d7277b200
Syed Ashhad <syedashad17@gmail.com>
2024-05-30 09:02:28 +0500

    Globe Removed

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -1,13 +1,11 @@
 import { title } from "@/components/primitives";
 import MainHeadingContactUs from "./MainHeadingContactUs";
-import { Globe } from "./Globe";
 import { ContactUsForm } from "./From";
 
 export default function contectUsPage() {
   return (
     <div>
       <MainHeadingContactUs/>
-      {/* <Globe/> */}
       <ContactUsForm/>
     </div>
   );
